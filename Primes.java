144250d25bf2b4b9ab48627556fa478abc5f02ca
Yuval <yuvalshavit9@gmail.com>
2024-12-01 14:18:27 +0200

    Updated files HW4

--- Primes.java
+++ Primes.java
@@ -1,5 +1,34 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int n = Integer.parseInt(args[0]);
+        boolean[] arr = new boolean[n];
+        for(int i = 2; i < n; i++){
+            arr[i] = true;
+        }
+
+        int i = 2;
+        while (i < n) {
+            int j = i + 1;
+            while(j < n){
+                if(j % i == 0){
+                    arr[j] = false;
+                }
+                j++;
+            }
+
+            i++;
+        }
+        System.out.println("Prime numbers up to " + n + ":");
+
+        int primeCount = 0;
+        for(int m = 0; m < n; m++){
+            if(arr[m] == true){
+                System.out.println(m);
+                primeCount++;
+            }
+        }
+        int percent = primeCount * 100 / n;
+        System.out.println("There are " + primeCount + " primes between 2 and " + n + " (" + percent + "% are primes)");
     }
+    
 }
